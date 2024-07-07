20cecf4acb15e7f70973236fcbca6e0d8f6b9527
nextredo <kb7737@gmail.com>
2024-07-07 21:07:59 +1000

    refactor(nvim): move nvim opts to vimrc

--- lua/options.lua
+++ lua/options.lua
@@ -1,42 +1,20 @@
 require "nvchad.options"
 
 -- Things to counteract wack NvChad settings
-vim.cmd([[
-    set shiftwidth=4
-    set tabstop=4
-    set softtabstop=4
-
-    " Default clipboard
-    set clipboard="autoselect,exclude:cons|linux"
-
-    set nowrap
-
-    " Render leading whitespace & invisble characters
-    set listchars+=multispace:·
-    set listchars+=lead:·
-    set listchars+=leadmultispace:·
-    set listchars+=trail:·
-    set listchars+=tab:→▹
-    set listchars+=extends:>
-    set listchars+=precedes:<
-    set listchars+=conceal:⭓
-    set listchars+=nbsp:▵
-    set list
-]])
+-- can be found both in ~/.vimrc and here
 
 -- Steal basic settings from vimrc
 vim.cmd("source ~/.vimrc")
 
--- Good: Desert, evening, slate, sorbet
--- Ok: Zaibatsu
+-- Override colorscheme in vimrc
 vim.cmd([[ colorscheme retrobox ]])
 
 -- https://vi.stackexchange.com/questions/37421/how-to-remove-neovim-trailing-white-space
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = {"*"},
-    callback = function()
-      local save_cursor = vim.fn.getpos(".")
-      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
-      vim.fn.setpos(".", save_cursor)
-    end,
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+--     pattern = {"*"},
+--     callback = function()
+--       local save_cursor = vim.fn.getpos(".")
+--       pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+--       vim.fn.setpos(".", save_cursor)
+--     end,
+-- })
